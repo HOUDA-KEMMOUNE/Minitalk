783139a0cefcf26b921a77d51e5ea5646619e6c9
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2025-03-10 02:24:44 +0000

    minitalk_bonus

---
+++ client_bonus.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 02:22:59 by hkemmoun          #+#    #+#             */
+/*   Updated: 2025/03/10 02:23:00 by hkemmoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	print_bits(char c, pid_t server_pid)
+{
+	int	i;
+	int	shifted;
+	int	error;
+	int	bit;
+
+	i = 7;
+	error = 0;
+	while (i >= 0)
+	{
+		shifted = 1 << i;
+		bit = c & shifted;
+		if (bit == 0)
+			error = kill(server_pid, SIGUSR1);
+		else
+			error = kill(server_pid, SIGUSR2);
+		if (error == -1)
+			print_error();
+		usleep(500);
+		i--;
+	}
+}
+
+void	send_char_by_char(char *message, pid_t server_pid)
+{
+	int	i;
+
+	i = 0;
+	while (message[i])
+	{
+		print_bits(message[i], server_pid);
+		i++;
+	}
+	print_bits('\0', server_pid);
+}
+
+int	main(int ac, char **av)
+{
+	pid_t	server_pid;
+	char	*message;
+
+	if (ac == 3)
+	{
+		server_pid = ft_atoi(av[1]);
+		if (server_pid <= 0 || server_pid > 2147483647)
+		{
+			ft_putstr("Invalid PID\n");
+			exit (1);
+		}
+		message = av[2];
+		send_char_by_char(message, server_pid);
+	}
+	else
+		printf("ERROR: Usage --> %s <PID> <message>\n", av[0]);
+}
---
+++ server_bonus.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 02:23:13 by hkemmoun          #+#    #+#             */
+/*   Updated: 2025/03/10 02:23:15 by hkemmoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	send_character(int *len, char *letter)
+{
+	if (*letter == '\0')
+	{
+		write(1, "\0\n", 2);
+		*len = 0;
+		*letter = 0;
+	}
+	write(1, letter, 1);
+	*len = 0;
+	*letter = 0;
+}
+
+void	handler(int sig_nbr, siginfo_t *info, void *context)
+{
+	static char	letter;
+	static int	bit;
+	static int	len;
+	static int	pid;
+
+	(void)context;
+	if (!pid)
+		pid = info->si_pid;
+	if (pid != info->si_pid)
+	{
+		pid = info->si_pid;
+		len = 0;
+		letter = 0;
+	}
+	if (sig_nbr == SIGUSR1)
+		bit = 0;
+	else
+		bit = 1;
+	letter = (letter << 1) | (bit & 1);
+	len++;
+	if (len == 8)
+		send_character(&len, &letter);
+}
+
+int	main(void)
+{
+	int					id;
+	struct sigaction	move;
+
+	id = getpid();
+	ft_putnbr(id);
+	write(1, "\n", 1);
+	move.sa_sigaction = handler;
+	move.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &move, NULL);
+	sigaction(SIGUSR2, &move, NULL);
+	while (1)
+		pause();
+	return (0);
+}
